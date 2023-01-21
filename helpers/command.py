ee8be8df530f9e5ffb22c1c8b624e29107a00948
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-01-21 17:55:41 +0100

    [IMP] command: update help text

--- helpers/command.py
+++ helpers/command.py
@@ -4,7 +4,7 @@ import argparse
 class Command(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.add_argument("type", help="Type de visuel (results, lineup, presentation)")
+        self.add_argument("type", help="Type de visuel (results, lineup, presentation, fastest, details)")
         self.add_argument("-s", "--sheet", help="Name of the Excel sheet to use", dest='sheet', default='Race 1')
         self.add_argument("-o", "--output", help="Output file to use", dest='output', default=None)
         self.add_argument("-i", "--input", help="Input file to use", dest='input', default='./data.xlsx')
